55df6d3802e6ecdd9d87eaf64b334263ec8d09b4
MARIA <cg3023141@labA205m17.IFSP>
2024-07-31 22:14:45 -0300

    starting server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,19 @@
-console.log('Hello, World!')
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+app.get('/user', (req, res) => {
+    res.json({
+        nome: "Maria",
+        email: "mariaeduardadefaria15@gmail.com"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
