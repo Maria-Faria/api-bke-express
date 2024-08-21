c12a5af31e20132b5a05e8f012526b26549a1943
MARIA <cg3023141@labA205m17.IFSP>
2024-08-21 20:39:47 -0300

    adding environment variables

---
+++ src/config.js
@@ -0,0 +1,4 @@
+export const PORT = process.env.PORT || 3000;
+export const HOST = process.env.HOST || 'http://localhost';
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const ENVIRONMENT = process.env.ENVIRONMENT;
--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
 import user from "./testeExport.js";
 import { sum, address } from './testeExport.js';
+import { PORT, HOST, ENVIRONMENT } from './config.js';
 
 const app = express();
-const port = 3000;
 
 app.use('/user', userRouter);
 app.use('/product', productRouter);
@@ -14,8 +14,6 @@ app.get('/', (req, res) => {
     res.send('Hello, world');
 })
 
-app.listen(port, () => {
-    console.log(`Servidor rodando em http://localhost:${user.name}`);
-    console.log(address);
-    console.log(sum(2, 30));
+app.listen(PORT, () => {
+    console.log(`Servidor rodando no ambiente ${ENVIRONMENT} em ${ENVIRONMENT == 'production' ? HOST : HOST + ':' + PORT }`);
 });
