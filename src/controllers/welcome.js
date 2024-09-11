88e7884c501abe9bf56fabb1beecaca69d24a606
MARIA <cg3023141@labA205m17.IFSP>
2024-09-11 20:18:39 -0300

    using logger

---
+++ src/controllers/welcome.js
@@ -0,0 +1,5 @@
+const welcome = (req, res) => {
+    res.json({message: "Seja bem vindo(a)!"})
+};
+
+export default welcome;
--- src/server.js
+++ src/server.js
@@ -1,20 +1,21 @@
 import express from 'express';
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
-import user from "./testeExport.js";
-import { sum, address } from './testeExport.js';
 import { PORT, HOST, ENVIRONMENT } from './config.js';
+import notFound from './controllers/notFound.js';
+import logger from './middlewares/logger.js';
+import welcome from './controllers/welcome.js';
 
 const app = express();
 
+app.use(logger);
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json({message: "Bem vindo à API!"})
-});
+app.get('/', welcome);
 
 app.use('/user', userRouter);
 app.use('/product', productRouter);
+app.use('*', notFound);
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando no ambiente ${ENVIRONMENT} em ${ENVIRONMENT == 'production' ? HOST : HOST + ':' + PORT }`);
